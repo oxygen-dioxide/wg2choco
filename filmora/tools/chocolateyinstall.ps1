54f76915f0b3adb35eb258d5ea1092c570900a08
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2023-03-14 12:00:34 +0800

    admin support

---
+++ filmora/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+ 
+$url        = 'https://download.wondershare.com/cbs_down/filmora_64bit_12.0.21_full846.exe'
+$packageArgs = @{
+  packageName   = 'filmora'
+  url           = $url
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+  softwareName  = 'filmora*'
+  checksum      = '396BE3C4C84EF39A79FEA630A1EEA02BE14C0A3DE8576C52D0B2217A53C60692'
+  checksumType  = 'sha256'
+  silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
--- wg2choco.py
+++ wg2choco.py
@@ -1,16 +1,26 @@
 import os
 import sys
 import yaml
-from   typing import Optional
+from   typing import List, Dict, Optional
 import py_linq
 import pathlib
+import argparse
 import packaging.version
 import github_action_utils
 
+#解析命令行
+argparser=argparse.ArgumentParser(description="Convert winget package to choco package")
+argparser.add_argument("winget_id",help="winget package id")
+argparser.add_argument("choco_id",help="choco package id")
+argparser.add_argument("--admin",help="should the package be installed with admin privilege",action="store_true")
+args = argparser.parse_args()
+
+winget_id:List[str] = args.winget_id.split(".")#winget包名，转为字符串列表形式
+chocoid:str = args.choco_id#choco包名
+admin:bool = args.admin#是否需要管理员权限
+
 #获取winget路径
-winget_id=sys.argv[1]#winget包名
-winget_id=winget_id.split(".")#转为字符串列表形式
-rootpath=pathlib.Path(sys.argv[0]).resolve().parent
+rootpath = pathlib.Path(sys.argv[0]).resolve().parent
 winget_software_path=rootpath.joinpath("winget-pkgs","manifests",winget_id[0][0].lower(),*winget_id)
 
 #最新版本号
@@ -29,7 +39,7 @@ try:
 except:
     pass
 winget_package_path=winget_software_path.joinpath(version)
-print("Winget package is located in",winget_package_path)
+print("Winget package is located in:",winget_package_path)
 
 #解析winget包
 for filepath in winget_package_path.iterdir():
@@ -42,18 +52,23 @@ for filepath in winget_package_path.iterdir():
     else:
         basicinfo=yaml.load(filepath.open(encoding="utf8"),Loader=yaml.BaseLoader)
 #在choco包中需要用到的信息
-chocoid=sys.argv[2]#choco包名
-chocoinfo={
+tags:List[str] = localeinfo.get("Tags",[])
+
+if(args.admin):
+    tags.append("admin")
+
+chocoinfo:Dict[str,str] = {
     "id":chocoid,
     "version":version,
     "title":localeinfo["PackageName"],
     "projectUrl":localeinfo["PackageUrl"],
-    "tags":" ".join(localeinfo.get("Tags",[])),
+    "tags":" ".join(tags),
     "summary":localeinfo["ShortDescription"],
     "description":localeinfo["ShortDescription"],
     "installerUrl":installerinfo["Installers"][0]["InstallerUrl"],
     "installerSha256":installerinfo["Installers"][0]["InstallerSha256"],
 }
+
 if("Author" in localeinfo):
     chocoinfo["authors"]=localeinfo["Author"]
 else:
@@ -70,9 +85,12 @@ if("InstallerSwitches" in installer):
         chocoinfo["silentArgs"]=installer["InstallerSwitches"]["SilentWithProgress"]
     else:
         chocoinfo["silentArgs"]=installer["InstallerSwitches"]["Silent"]
+
+print("Installer type:", installertype)
+print("admin:", args.admin)
 templatepath=rootpath.joinpath("templates",installertype)
 choco_package_path=pathlib.Path.cwd().joinpath(chocoid)
-print("Choco package is output to:",choco_package_path)
+print("Choco package is output to:", choco_package_path)
 
 #递归，把template_current_path的全部文件填入信息后，存入choco_current_path
 def folderwalk(
